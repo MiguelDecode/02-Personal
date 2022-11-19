b77adf1b634974f57c63351c8fb8a92b8d146ba1
migueldecode <migueldecode@gmail.com>
2022-11-19 16:05:40 +0100

    Save wallpapers

---
+++ vim_exercises/test.js
@@ -0,0 +1,20 @@
+function printTest() {
+	if(true) {
+		console.log('printTest')
+	}
+}
+
+// yank (copiar)
+yank
+(copiar)
+yank (copiar)
+// paste (pegar)
+
+
+// paste (pegar)
+console.log("printTest")
+function printTest() {
+	console.log("Hello world");
+}
+
+
