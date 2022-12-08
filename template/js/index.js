abaae32af55d58dd4e78d2b288e97275a516938d
migueldecode <migueldecode@gmail.com>
2022-12-08 18:18:45 +0100

    Add some wallpapers

---
+++ template/js/index.js
@@ -0,0 +1,23 @@
+function fitsInOneBox(boxes) {
+  
+boxes.forEach(box => {
+  if(box[0] > )
+})   
+
+
+ boxes.forEach(box => {
+  let values = Object.values(box)
+  console.log(values)
+ })
+
+  return false
+}
+
+const boxes = [
+  { l: 1, w: 1, h: 1 },
+  { l: 3, w: 3, h: 3 },
+  { l: 2, w: 2, h: 2 }
+]
+
+console.log(fitsInOneBox(boxes)) // true
+
